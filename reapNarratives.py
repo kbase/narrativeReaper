0331bfa3c47407f5cd1c8a8411566c711872d94c
kkellerlbl <kkeller@lbl.gov>
2019-04-02 15:25:41 -0700

    Update reapNarratives.py

--- reapNarratives.py
+++ reapNarratives.py
@@ -3,7 +3,6 @@
 import requests
 import sys
 import json
-import pickle
 
 import urllib3
 # there is a way to only disable InsecurePlatformWarning but I can't find it now
@@ -13,15 +12,8 @@ def get_proxy_map(proxyMapUrl):
     proxy_map = requests.get(proxyMapUrl)
     return proxy_map.json()
 
-def pickleFile(data,filename):
-    pickleFh = open (filename, 'w')
-    pickle.dump(data , pickleFh )
-    pickleFh.close()
-
 def main():
     proxyMapUrl='https://next.kbase.us/proxy_map'
-    pickleFilename='pickle.python'
-    pickleFile(get_proxy_map(proxyMapUrl),pickleFilename)
 
 if __name__ == "__main__":
     main()

7fc2ffed58532e8f0f9516ebe020c1b81f1687ee
kkellerlbl <kkeller@lbl.gov>
2019-04-02 11:40:15 -0700

    Update reapNarratives.py

--- reapNarratives.py
+++ reapNarratives.py
@@ -13,15 +13,15 @@ def get_proxy_map(proxyMapUrl):
     proxy_map = requests.get(proxyMapUrl)
     return proxy_map
 
-def pickleFile(filename):
+def pickleFile(data,filename):
     pickleFh = open (filename, 'w')
-    pickle.dump(get_proxy_map(proxyMapUrl) , pickleFh )
+    pickle.dump(data , pickleFh )
     pickleFh.close()
 
 def main():
     proxyMapUrl='https://next.kbase.us/proxy_map'
     pickleFilename='pickle.python'
-    pickleFile(pickleFilename)
+    pickleFile(get_proxy_map(proxyMapUrl),pickleFilename)
 
 if __name__ == "__main__":
     main()

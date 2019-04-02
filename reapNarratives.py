3bade19bdcf0452e8cda280366c7233cf62c04ad
kkellerlbl <kkeller@lbl.gov>
2019-04-02 15:44:22 -0700

    Update reapNarratives.py

--- reapNarratives.py
+++ reapNarratives.py
@@ -10,23 +10,27 @@ import urllib3
 # there is a way to only disable InsecurePlatformWarning but I can't find it now
 urllib3.disable_warnings()
 
+pp = pprint.PrettyPrinter(indent=4)
+
 def est_connections():
-    return psutil.net_connections()
+    allConnections = psutil.net_connections()
+    for conn in allConnections:
+        pp.pprint(conn)
 
 def get_proxy_map(proxyMapUrl):
     proxy_map = requests.get(proxyMapUrl)
     return proxy_map.json()
 
 def marker(proxyMap):
+    estConnections = est_connections()
     for session in proxyMap:
         print session['last_ip']
 
 def main():
-    pp = pprint.PrettyPrinter(indent=4)
     proxyMapUrl='https://next.kbase.us/proxy_map'
     marker(get_proxy_map(proxyMapUrl))
 #    pp.pprint(get_proxy_map(proxyMapUrl))
-    pp.pprint(est_connections())
+#    pp.pprint(est_connections())
 
 if __name__ == "__main__":
     main()

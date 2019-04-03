7b3f52d2c4f4af8337703bc56951f20628def74b
kkellerlbl <kkeller@lbl.gov>
2019-04-03 11:40:58 -0700

    Update reapNarratives.py

--- reapNarratives.py
+++ reapNarratives.py
@@ -35,6 +35,7 @@ def shutdown_session(url,sessionId):
 def est_connections(connectionMap,containerName):
 
     timestamp = time.time()
+    connectionMap = {}
 
     # would prefer to use docker lib but not working at the moment
     netstatOut = subprocess.check_output(['docker','exec',containerName,'netstat','-nt'])
@@ -52,10 +53,10 @@ def get_proxy_map(proxyMapUrl):
     proxy_map = requests.get(proxyMapUrl)
     return proxy_map.json()
 
-def marker(proxyMap,shutdownUrl,estConnections,timeout):
+def marker(currentProxyMap, localProxyMap, shutdownUrl,estConnections,timeout):
     now = time.time()
 
-    for session in proxyMap:
+    for session in currentProxyMap:
         # skip provisioned containers
         if session['state'] == 'queued':
             continue
@@ -65,18 +66,18 @@ def marker(proxyMap,shutdownUrl,estConnections,timeout):
                 print session['session_id'] + ' in estConnections to be timed out ' + str(sessionAge) + ' seconds old'
                 if shutdown_session(shutdownUrl,session['session_id']):
                     # pop returns the value and removes it from the dict
-                    proxyMap.pop(session['proxy_target'])
+                    localProxyMap.pop(session['proxy_target'])
                 else:
                     sys.stderr.write("unable to delete session " + session['session_id'] + " !\n")
             else:
                 print session['session_id'] + ' in estConnections ' + str(sessionAge) + ' seconds old, updating local map'
-                proxyMap[session['session_id']] = session
-                proxyMap[session['session_id']]['age'] = sessionAge
+                localProxyMap[session['session_id']] = session
+                localProxyMap[session['session_id']]['age'] = sessionAge
         else:
             print session['session_id'] + ' not in estConnections, creating dummy value'
 #            estConnections[session['proxy_target']] = now - 300
 
-    return proxyMap
+    return localProxyMap
 
 def main():
     # needed only to initialize
@@ -98,8 +99,8 @@ def main():
 # save pickle file
 
     oldProxyMap = read_pickle_data(pickleFile)
-    estConnections = est_connections(oldProxyMap, nginxContainerName)
-    newProxyMap = marker(get_proxy_map(proxyMapUrl), shutdownUrl, estConnections, int(timeout))
+    estConnections = est_connections(nginxContainerName)
+    newProxyMap = marker(get_proxy_map(proxyMapUrl), oldProxyMap, shutdownUrl, estConnections, int(timeout))
     save_pickle_data(newProxyMap, pickleFile)
 #    pp.pprint(get_proxy_map(proxyMapUrl))
 #    pp.pprint(est_connections())
